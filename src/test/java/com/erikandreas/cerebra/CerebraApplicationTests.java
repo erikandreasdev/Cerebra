af643c223ed587ece8f84e5be70e1112b485d6e4
Erik Andreas Barreto de Vera <erik.dvera+dev@proton.me>
2024-11-10 09:40:40 +0000

    Add Spring Boot project setup with Gradle
    Initialized a Spring Boot project with Gradle. Configured PostgreSQL for data persistence and included essential plugins and dependencies. Added Docker Compose and Git configuration files for streamlined development and deployment.

---
+++ src/test/java/com/erikandreas/cerebra/CerebraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erikandreas.cerebra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CerebraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
